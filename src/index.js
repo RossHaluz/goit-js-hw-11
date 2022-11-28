af5b96cbf2676060f87e77cfaa511f120e30d3ce
Rostyslav Haluzinskiy <rossgaluzinskiy@gmail.com>
2022-11-28 18:55:06 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,66 +1,65 @@
 import Notiflix from 'notiflix';
 import './css/styles.css';
-import FetchApiService from './js/news-service'
+import FetchApiService from './js/news-service';
 
 const form = document.querySelector('.search-form');
 const gallery = document.querySelector('.gallery');
 const loadMore = document.querySelector('.load-more');
 
 const fetchApiService = new FetchApiService();
-let totalHits = 0;
-
+let imagesNumber = 0;
 
 form.addEventListener('submit', submitForm);
 loadMore.addEventListener('click', onClickLoadMore);
 
 function submitForm(evt) {
   evt.preventDefault();
-  console.log(fetchApiService.number)
   fetchApiService.query = evt.currentTarget.elements.searchQuery.value.trim();
   if (!fetchApiService.query) {
-    clearPage()
-   return Notiflix.Notify.failure('Please enter something!');
+    clearPage();
+    return Notiflix.Notify.failure('Please enter something!');
   }
-
+  loadMore.style.display = 'block';
   fetchApiService.resetPage();
   clearPage();
-
+  imagesNumber = 0;
   fetchApiService.fetchImages().then(item => {
-    const {hits, totalHits} = item.data
-    if (!totalHits) {
-       Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");
-      loadMore.classList.add('is-hidden')
-      return
-    }
-    loadMore.classList.remove('is-hidden')
-    console.log(item)
-Notiflix.Notify.success(`Success, find ${totalHits} images`);
-    renderMarkup(hits)
+      const { hits, totalHits } = item.data;
+     
+      if (!totalHits) {
+        loadMore.classList.add('is-hidden');
+        return Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+      }
+     loadMore.classList.remove = 'is-hidden';
+    imagesNumber = totalHits;
+    imagesNumber -= hits.length;
 
-
-  }).catch(err => console.log(err))
+      if (imagesNumber === 0) {
+        loadMore.style.display = 'none';
+      }
+      Notiflix.Notify.success(`Success, find ${totalHits} images`);
+      renderMarkup(hits);
+    })
+    .catch(err => console.log(err));
 }
 
 function onClickLoadMore() {
   fetchApiService.fetchImages().then(item => {
-    const { hits, totalHits } = item.data;
-    fetchApiService.incrementPage()
-    renderMarkup(hits)
-console.log(fetchApiService.number)
-
-
-    const mathNumber = Math.round(totalHits / 40);
-
-    if (fetchApiService.number === mathNumber) {
-      loadMore.classList.add('is-hidden');
+    const { hits } = item.data;
+    renderMarkup(hits);
+    imagesNumber -= hits.length;
+    if (imagesNumber === 0|| hits.length < 40) {
+      loadMore.style.display = 'none';
     }
-
-
   });
 }
 
 function renderMarkup(arr) {
-  const markup = arr.map(({ webformatURL, tags, likes, views, comments, downloads }) => `
+  const markup = arr
+    .map(
+      ({ webformatURL, tags, likes, views, comments, downloads }) => `
   <div class="photo-card">
   <img src="${webformatURL}" alt="${tags}" loading="lazy" width="300"/>
   <div class="info">
@@ -82,11 +81,13 @@ function renderMarkup(arr) {
     </p>
   </div>
 </div>
-  `).join('')
+  `
+    )
+    .join('');
 
-  gallery.insertAdjacentHTML('beforeend', markup)
+  gallery.insertAdjacentHTML('beforeend', markup);
 }
 
 function clearPage() {
-  gallery.innerHTML = "";
+  gallery.innerHTML = '';
 }
